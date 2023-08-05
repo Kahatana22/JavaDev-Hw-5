6d33204089d8f6087ad3b8e03ca046bf96ccea35
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-08-05 22:59:39 +0300

    Create Client.java

---
+++ src/main/java/org/example/entities/Client.java
@@ -0,0 +1,13 @@
+package org.example.entities;
+
+public class Client {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Client(String name) {
+        this.name = name;
+    }
+}
